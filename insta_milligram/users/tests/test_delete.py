333477ecda69e1fb9b1c9e9e068a600aa4c24183
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-08 11:39:07 +0530

    fix: allow users to delete only their accounts

--- insta_milligram/users/tests/test_delete.py
+++ insta_milligram/users/tests/test_delete.py
@@ -1,3 +1,4 @@
+import django.http.response as drs
 import django.test as dt
 import django.urls as du
 import django.contrib.auth.models as dam
@@ -6,6 +7,10 @@ import rest_framework.status as rs  # type: ignore
 
 
 class TestView(dt.TestCase):
+    def generate_headers(self, login_response: drs.HttpResponse):
+        access_token = login_response.data["tokens"]["access"]  # type: ignore
+        return {"Authorization": f"Bearer {access_token}"}
+
     def setUp(self):
         self.TEST_REQUEST = {
             "username": "test",
@@ -14,7 +19,17 @@ class TestView(dt.TestCase):
             "first_name": "test",
             "last_name": "test",
         }
+        self.TEST_REQUEST_1 = {
+            "username": "test1",
+            "password": "testpass1",
+            "email": "test1@test.com",
+            "first_name": "test1",
+            "last_name": "test1",
+        }
+        self.LOGIN_REQUEST = {"username": "test", "password": "testpass"}
         self.USER_ID = 1
+        self.INCORRECT_USER_ID = 2
+        self.EXPIRED_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE1MTQ3MzU5LCJpYXQiOjE3MTUxNDcwNTksImp0aSI6IjYxNzlkMGQ5NTk1MTQ3NTdiMGU5YTA4ZjQ2YmRiMDY5IiwidXNlcl9pZCI6MX0.0q-rm-CvDISZyR4Pksfv5Ik00ltAyV5IK2SAsHb1KaI"
 
     def test_without_id(self):
         response = self.client.delete(du.reverse("users"))
@@ -24,9 +39,89 @@ class TestView(dt.TestCase):
             "User ID Missing",
         )
 
-    def test_wrong_id(self):
+    def test_correct(self):
+        self.client.post(du.reverse("users"), self.TEST_REQUEST)
+        login_response = self.client.post(
+            du.reverse("auths"),
+            self.LOGIN_REQUEST,
+            QUERY_STRING="action=generate",
+        )
+        access_token = login_response.data["tokens"]["access"]  # type: ignore
         response = self.client.delete(
             du.reverse("users_id", args=[self.USER_ID]),
+            headers={"Authorization": f"Bearer {access_token}"},  # type: ignore
+        )
+        self.assertEqual(response.status_code, rs.HTTP_200_OK)
+        self.assertEqual(
+            response.data["message"],  # type: ignore
+            "Success",
+        )
+        self.assertEqual(len(dam.User.objects.all()), 0)
+
+    def test_without_token(self):
+        self.client.post(du.reverse("users"), self.TEST_REQUEST)
+        login_response = self.client.post(
+            du.reverse("auths"),
+            self.LOGIN_REQUEST,
+            QUERY_STRING="action=generate",
+        )
+        access_token = login_response.data["tokens"]["access"]  # type: ignore
+        response = self.client.delete(
+            du.reverse("users_id", args=[self.USER_ID]),
+        )
+        self.assertEqual(response.status_code, rs.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(
+            response.data["message"],  # type: ignore
+            "Token Missing",
+        )
+        self.assertEqual(len(dam.User.objects.all()), 1)
+
+    def test_incorrect_token(self):
+        self.client.post(du.reverse("users"), self.TEST_REQUEST)
+        login_response = self.client.post(
+            du.reverse("auths"),
+            self.LOGIN_REQUEST,
+            QUERY_STRING="action=generate",
+        )
+        access_token = login_response.data["tokens"]["access"]  # type: ignore
+        response = self.client.delete(
+            du.reverse("users_id", args=[self.USER_ID]),
+            headers={"Authorization": f"Bearer {access_token}a"},  # type: ignore
+        )
+        self.assertEqual(response.status_code, rs.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(
+            response.data["message"],  # type: ignore
+            "Invalid Token",
+        )
+        self.assertEqual(len(dam.User.objects.all()), 1)
+
+    def test_expired_token(self):
+        self.client.post(du.reverse("users"), self.TEST_REQUEST)
+        response = self.client.delete(
+            du.reverse("users_id", args=[self.USER_ID]),
+            headers={"Authorization": f"Bearer {self.EXPIRED_ACCESS_TOKEN}"},  # type: ignore
+        )
+        self.assertEqual(response.status_code, rs.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(
+            response.data["message"],  # type: ignore
+            "Invalid Token",
+        )
+        self.assertEqual(len(dam.User.objects.all()), 1)
+
+    def test_delete_twice(self):
+        self.client.post(du.reverse("users"), self.TEST_REQUEST)
+        login_response = self.client.post(
+            du.reverse("auths"),
+            self.LOGIN_REQUEST,
+            QUERY_STRING="action=generate",
+        )
+        self.client.delete(
+            du.reverse("users_id", args=[self.USER_ID]),
+            headers=self.generate_headers(login_response),  # type: ignore
+        )
+        response = self.client.delete(
+            du.reverse("users_id", args=[self.USER_ID]),
+            headers=self.generate_headers(login_response),  # type: ignore
         )
         self.assertEqual(response.status_code, rs.HTTP_404_NOT_FOUND)
         self.assertEqual(
@@ -34,14 +129,21 @@ class TestView(dt.TestCase):
             "User Not Found",
         )
 
-    def test_correct(self):
+    def test_incorrect_id(self):
         self.client.post(du.reverse("users"), self.TEST_REQUEST)
-        response = self.client.delete(
-            du.reverse("users_id", args=[self.USER_ID]),
+        self.client.post(du.reverse("users"), self.TEST_REQUEST_1)
+        login_response = self.client.post(
+            du.reverse("auths"),
+            self.LOGIN_REQUEST,
+            QUERY_STRING="action=generate",
         )
-        self.assertEqual(response.status_code, rs.HTTP_200_OK)
+        response = self.client.delete(
+            du.reverse("users_id", args=[self.INCORRECT_USER_ID]),
+            headers=self.generate_headers(login_response),  # type: ignore
+        )
+        self.assertEqual(response.status_code, rs.HTTP_403_FORBIDDEN)
         self.assertEqual(
             response.data["message"],  # type: ignore
-            "Success",
+            "Operation Not Allowed",
         )
-        self.assertEqual(0, len(dam.User.objects.filter(pk=self.USER_ID)))
+        self.assertEqual(len(dam.User.objects.all()), 2)
--- insta_milligram/users/views/delete_view.py
+++ insta_milligram/users/views/delete_view.py
@@ -1,8 +1,10 @@
 import django.http.request as dr
-import django.contrib.auth.models as dam
 
 import rest_framework.response as rr  # type: ignore
 import rest_framework.status as rs  # type: ignore
+
+import rest_framework_simplejwt.authentication as ja
+import rest_framework_simplejwt.exceptions as je
 
 
 def delete(request: dr.HttpRequest, id: int = -1):
@@ -11,11 +13,26 @@ def delete(request: dr.HttpRequest, id: int = -1):
             {"message": "User ID Missing"},
             rs.HTTP_400_BAD_REQUEST,
         )
+    authentication = ja.JWTAuthentication()
     try:
-        dam.User.objects.get(pk=id).delete()
+        result = authentication.authenticate(request._request)  # type: ignore
+        if not result:
+            return rr.Response(
+                {"message": "Token Missing"},
+                rs.HTTP_401_UNAUTHORIZED,
+            )
+        user = result[0]  # type: ignore
+        if not user.id == id:  # type: ignore
+            return rr.Response(
+                {"message": "Operation Not Allowed"},
+                rs.HTTP_403_FORBIDDEN,
+            )
+        user.delete()  # type: ignore
         return rr.Response({"message": "Success"}, rs.HTTP_200_OK)
-    except dam.User.DoesNotExist:
+    except je.InvalidToken:
         return rr.Response(
-            {"message": "User Not Found"},
-            rs.HTTP_404_NOT_FOUND,
+            {"message": "Invalid Token"},
+            rs.HTTP_401_UNAUTHORIZED,
         )
+    except je.AuthenticationFailed:
+        return rr.Response({"message": "User Not Found"}, rs.HTTP_404_NOT_FOUND)
