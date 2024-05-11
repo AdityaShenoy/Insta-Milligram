f2bf7a87544f05fdddf3dc9294c0f9bfb529a1af
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-11 11:15:19 +0530

    test: helper function to test responses with no custom data

--- insta_milligram/insta_milligram/helpers.py
+++ insta_milligram/insta_milligram/helpers.py
@@ -14,6 +14,15 @@ def assertEqualResponse(
     assert response.status_code == status_code
 
 
+# TODO: use this function wherever possible
+def assertEqualResponses(
+    response1: dres.HttpResponse,
+    response2: dres.HttpResponse,
+):
+    assert response1.data == response2.data  # type: ignore
+    assert response1.status_code == response1.status_code
+
+
 def create_response(
     message: str,
     status_code: int,
