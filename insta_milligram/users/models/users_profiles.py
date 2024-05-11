c2ae7255ee3ef4e8f057470aa7fe24240310fb3e
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-11 22:08:25 +0530

    feat: add model for UsersProfiles

---
+++ insta_milligram/users/models/users_profiles.py
@@ -0,0 +1,12 @@
+import django.db.models as ddm
+import django.contrib.auth.models as dcam
+
+
+class UsersProfiles(ddm.Model):
+    user = ddm.ForeignKey(
+        dcam.User,
+        on_delete=ddm.CASCADE,
+        related_name="profile",
+    )
+    followers_count = ddm.IntegerField(default=0)
+    followings_count = ddm.IntegerField(default=0)
