59b0c2b582c068b4967258da3d240136e482cc5d
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-11 11:30:25 +0530

    refactor: move constants used in multiple files in constants section

--- insta_milligram/users/forms.py
+++ insta_milligram/users/forms.py
@@ -1,11 +1,11 @@
 import django.forms as df
-import django.core.validators as dv
+import django.core.validators as dcv
 
 
 class UserForm(df.Form):
     username = df.CharField(
         max_length=50,
-        validators=[dv.RegexValidator(r"[\w.]+")],
+        validators=[dcv.RegexValidator(r"[\w.]+")],
     )
     password = df.CharField(min_length=8, max_length=50)
     email = df.EmailField(max_length=50)
--- insta_milligram/users/tests/test_form.py
+++ insta_milligram/users/tests/test_form.py
@@ -1,35 +1,38 @@
 import django.test as dt
 
 from .. import forms as f
+import insta_milligram.constants as c
 
 
 class TestForm(dt.SimpleTestCase):
     def setUp(self):
-        self.TEST_REQUEST = {
-            "username": "test",
-            "password": "testpass",
-            "email": "test@test.com",
-            "first_name": "test",
-            "last_name": "test",
-        }
-        self.EMPTY_REQUEST = {k: "" for k in self.TEST_REQUEST}
-        self.BIG_REQUEST = {k: "a" * 51 for k in self.TEST_REQUEST}
-        self.SMALL_PWD_REQUEST = {**self.TEST_REQUEST, "password": "test"}
+        self.EMPTY_REQUEST = {k: "" for k in c.inputs.SIGNUP_REQUEST}
+        self.BIG_REQUEST = {k: "a" * 51 for k in c.inputs.SIGNUP_REQUEST}
+        self.SMALL_PWD_REQUEST = {**c.inputs.SIGNUP_REQUEST, "password": "test"}
 
     def test_missing_field(self):
         form = f.UserForm({})
         self.assertFalse(form.is_valid())
-        self.assertEqual(set(form.errors.keys()), set(self.TEST_REQUEST.keys()))
+        self.assertEqual(
+            set(form.errors.keys()),
+            set(c.inputs.SIGNUP_REQUEST.keys()),
+        )
 
     def test_empty(self):
         form = f.UserForm(self.EMPTY_REQUEST)
         self.assertFalse(form.is_valid())
-        self.assertEqual(set(form.errors.keys()), set(self.TEST_REQUEST.keys()))
+        self.assertEqual(
+            set(form.errors.keys()),
+            set(c.inputs.SIGNUP_REQUEST.keys()),
+        )
 
     def test_big(self):
         form = f.UserForm(self.BIG_REQUEST)
         self.assertFalse(form.is_valid())
-        self.assertEqual(set(form.errors.keys()), set(self.TEST_REQUEST.keys()))
+        self.assertEqual(
+            set(form.errors.keys()),
+            set(c.inputs.SIGNUP_REQUEST.keys()),
+        )
 
     def test_small_password(self):
         form = f.UserForm(self.SMALL_PWD_REQUEST)
@@ -37,11 +40,11 @@ class TestForm(dt.SimpleTestCase):
         self.assertIn("password", form.errors.keys())
 
     def test_username_with_special_chars(self):
-        form = f.UserForm({**self.TEST_REQUEST, "username": "@"})
+        form = f.UserForm({**c.inputs.SIGNUP_REQUEST, "username": "@"})
         self.assertFalse(form.is_valid())
         self.assertIn("username", form.errors.keys())
 
     def test_valid(self):
-        form = f.UserForm(self.TEST_REQUEST)
+        form = f.UserForm(c.inputs.SIGNUP_REQUEST)
         self.assertTrue(form.is_valid())
         self.assertEqual(0, len(form.errors))
