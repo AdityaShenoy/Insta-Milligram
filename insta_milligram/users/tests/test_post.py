4a29c5c3254000342d290e3ceec4c50585886a15
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-11 10:49:16 +0530

    refactor: move constants used in multiple files in constants section

--- insta_milligram/users/tests/test_post.py
+++ insta_milligram/users/tests/test_post.py
@@ -4,64 +4,63 @@ import django.contrib.auth.models as dam
 
 import rest_framework.status as rs  # type: ignore
 
+import insta_milligram.constants as c
+import insta_milligram.helpers as h
+
 
 class TestView(dt.TestCase):
-    def setUp(self):
-        self.TEST_REQUEST = {
-            "username": "test",
-            "password": "testpass",
-            "email": "test@test.com",
-            "first_name": "test",
-            "last_name": "test",
-        }
-        self.EMPTY_REQUEST = {k: "" for k in self.TEST_REQUEST}
-        self.BIG_REQUEST = {k: "a" * 51 for k in self.TEST_REQUEST}
-        self.SMALL_PWD_REQUEST = {**self.TEST_REQUEST, "password": "test"}
-
     def test_invalid(self):
         response = self.client.post(du.reverse("users"))
-        self.assertEqual(response.status_code, rs.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            response.data["message"],  # type: ignore
-            "Invalid Data",
+        h.assertEqualResponse(
+            response, c.messages.INVALID_DATA, rs.HTTP_400_BAD_REQUEST
         )
         self.assertEqual(
             set(response.data["errors"].keys()),  # type: ignore
-            set(self.TEST_REQUEST.keys()),
+            set(c.inputs.SIGNUP_REQUEST.keys()),
         )
 
     def test_valid(self):
-        response = self.client.post(du.reverse("users"), self.TEST_REQUEST)
-        self.assertEqual(response.status_code, rs.HTTP_200_OK)
-        self.assertEqual(
-            response.data["message"],  # type: ignore
-            "Success",
+        response = self.client.post(
+            du.reverse("users"),
+            c.inputs.SIGNUP_REQUEST,
         )
-        user = dam.User.objects.get(username=self.TEST_REQUEST["username"])
-        self.assertEqual(user.username, self.TEST_REQUEST["username"])
-        self.assertTrue(user.check_password(self.TEST_REQUEST["password"]))
-        self.assertEqual(user.first_name, self.TEST_REQUEST["first_name"])
-        self.assertEqual(user.last_name, self.TEST_REQUEST["last_name"])
-        self.assertEqual(user.email, self.TEST_REQUEST["email"])
+        h.assertEqualResponse(response, c.messages.SUCCESS, rs.HTTP_200_OK)
+        user = dam.User.objects.get(
+            username=c.inputs.SIGNUP_REQUEST["username"],
+        )
+        self.assertEqual(user.username, c.inputs.SIGNUP_REQUEST["username"])
+        self.assertTrue(
+            user.check_password(c.inputs.SIGNUP_REQUEST["password"]),
+        )
+        self.assertEqual(user.first_name, c.inputs.SIGNUP_REQUEST["first_name"])
+        self.assertEqual(user.last_name, c.inputs.SIGNUP_REQUEST["last_name"])
+        self.assertEqual(user.email, c.inputs.SIGNUP_REQUEST["email"])
 
     def test_twice_username(self):
         self.client.post(
-            du.reverse("users"), {**self.TEST_REQUEST, "email": "test1@test.com"}
+            du.reverse("users"),
+            {
+                **c.inputs.SIGNUP_REQUEST,
+                "email": "test1@test.com",
+            },
         )
-        response = self.client.post(du.reverse("users"), self.TEST_REQUEST)
-        self.assertEqual(response.status_code, rs.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            response.data["message"],  # type: ignore
-            "User Already Exists",
+        response = self.client.post(
+            du.reverse("users"),
+            c.inputs.SIGNUP_REQUEST,
+        )
+        h.assertEqualResponse(
+            response, c.messages.USER_ALREADY_EXISTS, rs.HTTP_400_BAD_REQUEST
         )
 
     def test_twice_email(self):
         self.client.post(
-            du.reverse("users"), {**self.TEST_REQUEST, "username": "test1"}
+            du.reverse("users"),
+            {**c.inputs.SIGNUP_REQUEST, "username": "test1"},
         )
-        response = self.client.post(du.reverse("users"), self.TEST_REQUEST)
-        self.assertEqual(response.status_code, rs.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            response.data["message"],  # type: ignore
-            "User Already Exists",
+        response = self.client.post(
+            du.reverse("users"),
+            c.inputs.SIGNUP_REQUEST,
+        )
+        h.assertEqualResponse(
+            response, c.messages.USER_ALREADY_EXISTS, rs.HTTP_400_BAD_REQUEST
         )
