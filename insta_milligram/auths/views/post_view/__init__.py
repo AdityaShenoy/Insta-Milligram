e3b5fce965f448da441c4816749747cf79970ee1
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-12 12:25:30 +0530

    refactor: move incorrect token parameter response to constants section

--- insta_milligram/auths/views/post_view/__init__.py
+++ insta_milligram/auths/views/post_view/__init__.py
@@ -1,23 +1,14 @@
 import django.http.request as dr
 
-import rest_framework.response as rr  # type: ignore
-import rest_framework.status as rs  # type: ignore
-
 from . import generate_tokens as g
 from . import refresh_tokens as r
+import insta_milligram.constants as ic
 
 
 def post(request: dr.HttpRequest):
     action = request.GET.get("action")
     if action not in ["generate", "refresh"]:
-        # todo: move this to constants
-        return rr.Response(
-            {
-                "message": "Incorrect Parameter - "
-                + "Expected ?action=generate or ?action=refresh"
-            },
-            rs.HTTP_400_BAD_REQUEST,
-        )
+        return ic.responses.INCORRECT_TOKEN_PARAMETER
     if action == "generate":
         return g.generate_tokens(request)
     return r.refresh_tokens(request)
