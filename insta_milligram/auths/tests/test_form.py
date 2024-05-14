96bf6ab9c403c2339577020c9c483900c6ec6039
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 22:32:14 +0530

    refactor: use request list instead of deprecated requests

--- insta_milligram/auths/tests/test_form.py
+++ insta_milligram/auths/tests/test_form.py
@@ -6,24 +6,33 @@ import insta_milligram.constants as ic
 
 class TestForm(dt.SimpleTestCase):
     def setUp(self):
-        self.EMPTY_REQUEST = {k: "" for k in ic.inputs.LOGIN_REQUEST}
-        self.BIG_REQUEST = {k: "a" * 51 for k in ic.inputs.LOGIN_REQUEST}
-        self.SMALL_PWD_REQUEST = {**ic.inputs.LOGIN_REQUEST, "password": "test"}
+        self.EMPTY_REQUEST = {k: "" for k in ic.inputs.LOGIN_REQUESTS[1]}
+        self.BIG_REQUEST = {k: "a" * 51 for k in ic.inputs.LOGIN_REQUESTS[1]}
+        self.SMALL_PWD_REQUEST = {
+            **ic.inputs.LOGIN_REQUESTS[1],
+            "password": "test",
+        }
 
     def test_missing_field(self):
         form = f.GenerateTokenForm({})
         assert not form.is_valid()
-        assert set(form.errors.keys()) == set(ic.inputs.LOGIN_REQUEST.keys())
+        assert set(form.errors.keys()) == set(
+            ic.inputs.LOGIN_REQUESTS[1].keys(),
+        )
 
     def test_empty(self):
         form = f.GenerateTokenForm(self.EMPTY_REQUEST)
         assert not form.is_valid()
-        assert set(form.errors.keys()) == set(ic.inputs.LOGIN_REQUEST.keys())
+        assert set(form.errors.keys()) == set(
+            ic.inputs.LOGIN_REQUESTS[1].keys(),
+        )
 
     def test_big(self):
         form = f.GenerateTokenForm(self.BIG_REQUEST)
         assert not form.is_valid()
-        assert set(form.errors.keys()) == set(ic.inputs.LOGIN_REQUEST.keys())
+        assert set(form.errors.keys()) == set(
+            ic.inputs.LOGIN_REQUESTS[1].keys(),
+        )
 
     def test_small_password(self):
         form = f.GenerateTokenForm(self.SMALL_PWD_REQUEST)
@@ -31,11 +40,13 @@ class TestForm(dt.SimpleTestCase):
         assert "password" in form.errors.keys()
 
     def test_username_with_special_chars(self):
-        form = f.GenerateTokenForm({**ic.inputs.LOGIN_REQUEST, "username": "@"})
+        form = f.GenerateTokenForm(
+            {**ic.inputs.LOGIN_REQUESTS[1], "username": "@"},
+        )
         assert not form.is_valid()
         assert "username" in form.errors.keys()
 
     def test_valid(self):
-        form = f.GenerateTokenForm(ic.inputs.LOGIN_REQUEST)
+        form = f.GenerateTokenForm(ic.inputs.LOGIN_REQUESTS[1])
         assert form.is_valid()
         assert len(form.errors) == 0
