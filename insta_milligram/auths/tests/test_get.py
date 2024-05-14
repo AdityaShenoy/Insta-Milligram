248445488c7ebb7f2dfc36b055a0a429058f6da2
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 19:28:17 +0530

    style: standardize import abbreviations and ordering

--- insta_milligram/auths/tests/test_get.py
+++ insta_milligram/auths/tests/test_get.py
@@ -1,66 +1,66 @@
 import django.test as dt
-import django.contrib.auth.models as dam
+import django.contrib.auth.models as dcam
 
-import insta_milligram.constants as c
-import insta_milligram.tests as t
+import insta_milligram.constants as ic
+import insta_milligram.tests as it
 
 
 class TestView(dt.TestCase):
     def test_without_id(self):
-        response = self.client.delete(c.urls.USERS)
-        t.assert_equal_responses(response, c.responses.USER_ID_MISSING)
+        response = self.client.delete(ic.urls.USERS)
+        it.assert_equal_responses(response, ic.responses.USER_ID_MISSING)
 
     def test_without_token(self):
-        self.client.post(c.urls.USERS, c.inputs.SIGNUP_REQUEST)
+        self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
         login_response = self.client.post(
-            c.urls.AUTHS,
-            c.inputs.LOGIN_REQUEST,
+            ic.urls.AUTHS,
+            ic.inputs.LOGIN_REQUEST,
             QUERY_STRING="action=generate",
         )
         access_token = login_response.data["tokens"]["access"]  # type: ignore
         response = self.client.delete(
-            c.urls.USERS_ID_1,
+            ic.urls.USERS_ID_1,
         )
-        t.assert_equal_responses(response, c.responses.TOKEN_MISSING)
-        assert len(dam.User.objects.all()) == 1
+        it.assert_equal_responses(response, ic.responses.TOKEN_MISSING)
+        assert len(dcam.User.objects.all()) == 1
 
     def test_incorrect_token(self):
-        self.client.post(c.urls.USERS, c.inputs.SIGNUP_REQUEST)
+        self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
         login_response = self.client.post(
-            c.urls.AUTHS,
-            c.inputs.LOGIN_REQUEST,
+            ic.urls.AUTHS,
+            ic.inputs.LOGIN_REQUEST,
             QUERY_STRING="action=generate",
         )
         access_token = login_response.data["tokens"]["access"]  # type: ignore
         response = self.client.delete(
-            c.urls.USERS_ID_1,
+            ic.urls.USERS_ID_1,
             headers={"Authorization": f"Bearer {access_token}a"},  # type: ignore
         )
-        t.assert_equal_responses(response, c.responses.INVALID_TOKEN)
-        assert len(dam.User.objects.all()) == 1
+        it.assert_equal_responses(response, ic.responses.INVALID_TOKEN)
+        assert len(dcam.User.objects.all()) == 1
 
     def test_expired_token(self):
-        self.client.post(c.urls.USERS, c.inputs.SIGNUP_REQUEST)
+        self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
         response = self.client.delete(
-            c.urls.USERS_ID_1,
-            headers={"Authorization": f"Bearer {c.inputs.EXPIRED_ACCESS_TOKEN}"},  # type: ignore
+            ic.urls.USERS_ID_1,
+            headers={"Authorization": f"Bearer {ic.inputs.EXPIRED_ACCESS_TOKEN}"},  # type: ignore
         )
-        t.assert_equal_responses(response, c.responses.INVALID_TOKEN)
-        assert len(dam.User.objects.all()) == 1
+        it.assert_equal_responses(response, ic.responses.INVALID_TOKEN)
+        assert len(dcam.User.objects.all()) == 1
 
     def test_delete_twice(self):
-        self.client.post(c.urls.USERS, c.inputs.SIGNUP_REQUEST)
+        self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
         login_response = self.client.post(
-            c.urls.AUTHS,
-            c.inputs.LOGIN_REQUEST,
+            ic.urls.AUTHS,
+            ic.inputs.LOGIN_REQUEST,
             QUERY_STRING="action=generate",
         )
         self.client.delete(
-            c.urls.USERS_ID_1,
-            headers=t.generate_headers(login_response),  # type: ignore
+            ic.urls.USERS_ID_1,
+            headers=it.generate_headers_old(login_response),  # type: ignore
         )
         response = self.client.delete(
-            c.urls.USERS_ID_1,
-            headers=t.generate_headers(login_response),  # type: ignore
+            ic.urls.USERS_ID_1,
+            headers=it.generate_headers_old(login_response),  # type: ignore
         )
-        t.assert_equal_responses(response, c.responses.USER_NOT_FOUND)
+        it.assert_equal_responses(response, ic.responses.USER_NOT_FOUND)
