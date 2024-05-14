fa0138a5e74ac6ccff133d1fb0139953a4d0ecc4
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 14:50:25 +0530

    refactor: give self explanatory variable names

--- insta_milligram/users/views/post_view.py
+++ insta_milligram/users/views/post_view.py
@@ -15,9 +15,13 @@ import users.models.users_profiles as umup
 @ird.check_form(f.UserForm)
 def post(request: dhreq.HttpRequest):
     form_data = if_.get_data(f.UserForm(request.POST))
-    user1 = dcam.User.objects.filter(email=form_data["email"])
-    user2 = dcam.User.objects.filter(username=form_data["username"])
-    if len(user1) + len(user2):
+    existing_users_with_same_email = dcam.User.objects.filter(
+        email=form_data["email"],
+    )
+    exisitng_users_with_same_username = dcam.User.objects.filter(
+        username=form_data["username"],
+    )
+    if existing_users_with_same_email or exisitng_users_with_same_username:
         return ic.responses.USER_ALREADY_EXISTS
     else:
         with ddt.atomic():
