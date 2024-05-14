57e2fa3c309402cf21085a030bae0d55f287f175
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 20:41:16 +0530

    style: standardize import abbreviations and ordering

--- insta_milligram/users/tests/test_delete.py
+++ insta_milligram/users/tests/test_delete.py
@@ -1,30 +1,30 @@
 import django.test as dt
-import django.contrib.auth.models as dam
+import django.contrib.auth.models as dcam
 
-import insta_milligram.constants as c
-import insta_milligram.tests as t
+import insta_milligram.constants as ic
+import insta_milligram.tests as it
 
 
 class TestView(dt.TestCase):
     def setUp(self):
-        self.header = t.signup_and_login(
+        self.header = it.signup_and_login(
             self.client,
-            c.inputs.SIGNUP_REQUESTS[0],
+            ic.inputs.SIGNUP_REQUESTS[0],
         )
-        t.signup_and_login(self.client, c.inputs.SIGNUP_REQUESTS[1])
+        it.signup_and_login(self.client, ic.inputs.SIGNUP_REQUESTS[1])
 
     def test_correct(self):
         response = self.client.delete(
-            c.urls.USERS_ID_1,
+            ic.urls.USERS_ID_1,
             headers=self.header,  # type: ignore
         )
-        t.assert_equal_responses(response, c.responses.SUCCESS)
-        assert len(dam.User.objects.all()) == 1
+        it.assert_equal_responses(response, ic.responses.SUCCESS)
+        assert len(dcam.User.objects.all()) == 1
 
     def test_incorrect_id(self):
         response = self.client.delete(
-            c.urls.USERS_ID_2,
+            ic.urls.USERS_ID_2,
             headers=self.header,  # type: ignore
         )
-        t.assert_equal_responses(response, c.responses.OPERATION_NOT_ALLOWED)
-        assert len(dam.User.objects.all()) == 2
+        it.assert_equal_responses(response, ic.responses.OPERATION_NOT_ALLOWED)
+        assert len(dcam.User.objects.all()) == 2
