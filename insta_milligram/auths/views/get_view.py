4d9999250b978a25f809481a6eef4a2cdf8dcdf6
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 15:26:09 +0530

    style: standardize import abbreviations and ordering

--- insta_milligram/auths/views/get_view.py
+++ insta_milligram/auths/views/get_view.py
@@ -1,24 +1,21 @@
 import django.http.request as dhreq
 
-import rest_framework.status as rs  # type: ignore
-
 import rest_framework_simplejwt.authentication as ja
 import rest_framework_simplejwt.exceptions as je
 
-
-import insta_milligram.constants as c
-import insta_milligram.responses as r
+import insta_milligram.constants as ic
+import insta_milligram.responses as ir
 
 
 def get(request: dhreq.HttpRequest, id: int):
     if id == -1:
-        return c.responses.USER_ID_MISSING
+        return ic.responses.USER_ID_MISSING
     try:
         result = ja.JWTAuthentication().authenticate(request._request)  # type: ignore
         if result:
-            return r.create_response(c.responses.SUCCESS, {"user": result[0]})
-        return c.responses.TOKEN_MISSING
+            return ir.create_response(ic.responses.SUCCESS, {"user": result[0]})
+        return ic.responses.TOKEN_MISSING
     except je.InvalidToken:
-        return c.responses.INVALID_TOKEN
+        return ic.responses.INVALID_TOKEN
     except je.AuthenticationFailed:
-        return c.responses.USER_NOT_FOUND
+        return ic.responses.USER_NOT_FOUND
