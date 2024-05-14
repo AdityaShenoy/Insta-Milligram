6328097928c307d0f850495d8d0a1f967d9a3a68
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 15:18:30 +0530

    style: standardize import abbreviations and ordering

--- insta_milligram/users/views/put_view.py
+++ insta_milligram/users/views/put_view.py
@@ -1,29 +1,27 @@
-import django.http.request as dr
-import django.contrib.auth.models as dam
-
-import rest_framework.status as rs  # type: ignore
+import django.contrib.auth.models as dcam
+import django.http.request as dhreq
 
 from .. import forms as f
-import insta_milligram.constants as c
-import insta_milligram.responses as r
+import insta_milligram.constants as ic
+import insta_milligram.responses as ir
 import insta_milligram.responses.decorators as ird
 
 
 @ird.check_missing_id()
-def put(request: dr.HttpRequest, id: int = -1):
+def put(request: dhreq.HttpRequest, id: int = -1):
     form = f.UserForm(request.data)  # type: ignore
     if not form.is_valid():
-        return r.create_response(
-            c.responses.INVALID_DATA,
+        return ir.create_response(
+            ic.responses.INVALID_DATA,
             {"errors": form.errors},
         )
     form_data = form.cleaned_data
     try:
-        user = dam.User.objects.get(pk=id)
+        user = dcam.User.objects.get(pk=id)
         for field in form_data:
             user.__setattr__(field, form_data[field])
         user.set_password(form_data["password"])
         user.save()
-        return c.responses.SUCCESS
-    except dam.User.DoesNotExist:
-        return c.responses.USER_NOT_FOUND
+        return ic.responses.SUCCESS
+    except dcam.User.DoesNotExist:
+        return ic.responses.USER_NOT_FOUND
