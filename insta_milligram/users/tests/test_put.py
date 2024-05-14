eb9ef378a9c1a8d9ec2218e9d5f27098e46213f6
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 20:19:29 +0530

    style: standardize import abbreviations and ordering

--- insta_milligram/users/tests/test_put.py
+++ insta_milligram/users/tests/test_put.py
@@ -1,10 +1,10 @@
+import django.contrib.auth.models as dcam
 import django.test as dt
-import django.contrib.auth.models as dam
 
 import rest_framework.test as rt  # type: ignore
 
-import insta_milligram.constants as c
-import insta_milligram.tests as t
+import insta_milligram.constants as ic
+import insta_milligram.tests as it
 
 
 class TestView(dt.TestCase):
@@ -12,27 +12,27 @@ class TestView(dt.TestCase):
         self.client = rt.APIClient()
 
     def test_without_id(self):
-        response = self.client.put(c.urls.USERS)
-        t.assert_equal_responses(response, c.responses.USER_ID_MISSING)
+        response = self.client.put(ic.urls.USERS)
+        it.assert_equal_responses(response, ic.responses.USER_ID_MISSING)
 
     def test_invalid(self):
-        response = self.client.put(c.urls.USERS_ID_1)
-        t.assert_equal_responses(response, c.responses.INVALID_DATA)
+        response = self.client.put(ic.urls.USERS_ID_1)
+        it.assert_equal_responses(response, ic.responses.INVALID_DATA)
         assert set(response.data["errors"].keys()) == set(  # type: ignore
-            c.inputs.SIGNUP_REQUEST.keys()
+            ic.inputs.SIGNUP_REQUEST.keys()
         )
 
     def test_valid(self):
-        self.client.post(c.urls.USERS, c.inputs.SIGNUP_REQUEST)
-        response = self.client.put(c.urls.USERS_ID_1, c.inputs.UPDATE_REQUEST)
-        t.assert_equal_responses(response, c.responses.SUCCESS)
-        user = dam.User.objects.get(pk=1)
-        for field in c.inputs.UPDATE_REQUEST:
+        self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
+        response = self.client.put(ic.urls.USERS_ID_1, ic.inputs.UPDATE_REQUEST)
+        it.assert_equal_responses(response, ic.responses.SUCCESS)
+        user = dcam.User.objects.get(pk=1)
+        for field in ic.inputs.UPDATE_REQUEST:
             if field == "password":
                 continue
-            assert user.__getattribute__(field) == c.inputs.UPDATE_REQUEST[field]
-        assert user.check_password(c.inputs.UPDATE_REQUEST["password"])
+            assert user.__getattribute__(field) == ic.inputs.UPDATE_REQUEST[field]
+        assert user.check_password(ic.inputs.UPDATE_REQUEST["password"])
 
     def test_without_user(self):
-        response = self.client.put(c.urls.USERS_ID_1, c.inputs.UPDATE_REQUEST)
-        t.assert_equal_responses(response, c.responses.USER_NOT_FOUND)
+        response = self.client.put(ic.urls.USERS_ID_1, ic.inputs.UPDATE_REQUEST)
+        it.assert_equal_responses(response, ic.responses.USER_NOT_FOUND)
