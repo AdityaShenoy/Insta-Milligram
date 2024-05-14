239be167420359a9a82f195a464c3975b42ca9e4
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 23:22:45 +0530

    feat: POST /auths?action=refresh

---
+++ insta_milligram/auths/tests/test_refresh.py
@@ -0,0 +1,45 @@
+import django.test as dt
+
+import insta_milligram.constants as ic
+import insta_milligram.tests as it
+
+
+class TestView(dt.TestCase):
+    def setUp(self):
+        self.DUMMY_REFRESH = {"refresh": "dummy"}
+        self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUESTS[1])
+        response = self.client.post(
+            ic.urls.AUTHS,
+            ic.inputs.LOGIN_REQUESTS[1],
+            QUERY_STRING="action=generate",
+        )
+        self.tokens: dict[str, str] = response.data["tokens"]  # type: ignore
+
+    def test_invalid(self):
+        response = self.client.post(
+            ic.urls.AUTHS,
+            QUERY_STRING="action=refresh",
+        )
+        it.assert_equal_responses(response, ic.responses.INVALID_DATA)
+        assert "refresh" in response.data["errors"]  # type: ignore
+
+    def test_invalid_token(self):
+        response = self.client.post(
+            ic.urls.AUTHS, self.DUMMY_REFRESH, QUERY_STRING="action=refresh"
+        )
+        it.assert_equal_responses(response, ic.responses.INVALID_TOKEN)
+
+    def test_expired_token(self):
+        response = self.client.post(
+            ic.urls.AUTHS,
+            ic.inputs.EXPIRED_REFRESH_TOKEN,
+            QUERY_STRING="action=refresh",
+        )
+        it.assert_equal_responses(response, ic.responses.INVALID_TOKEN)
+
+    def test_valid(self):
+        response = self.client.post(
+            ic.urls.AUTHS, self.tokens, QUERY_STRING="action=refresh"
+        )
+        it.assert_equal_responses(response, ic.responses.SUCCESS)
+        assert "access" in response.data  # type: ignore
--- insta_milligram/auths/views/post_view/refresh_tokens.py
+++ insta_milligram/auths/views/post_view/refresh_tokens.py
@@ -1,4 +1,21 @@
 import django.http.request as dhreq
 
+import rest_framework_simplejwt.views as jv
 
-def refresh_tokens(request: dhreq.HttpRequest): ...
+import auths.forms as af
+import insta_milligram.constants as ic
+import insta_milligram.responses as ir
+import insta_milligram.responses.decorators as ird
+
+
+@ird.check_form(af.RefreshTokenForm)
+def refresh_tokens(request: dhreq.HttpRequest):
+    response = jv.token_refresh(request._request)  # type: ignore
+    if ("code" in response.data) and (  # type: ignore
+        response.data["code"].code == "token_not_valid"  # type: ignore
+    ):
+        return ic.responses.INVALID_TOKEN
+    return ir.create_response(
+        ic.responses.SUCCESS,
+        response.data,  # type: ignore
+    )
