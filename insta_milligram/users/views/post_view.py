4bbd0adaddd79a8714135cae7a68d379062d8867
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 14:48:06 +0530

    style: standardize import abbreviations

--- insta_milligram/users/views/post_view.py
+++ insta_milligram/users/views/post_view.py
@@ -1,10 +1,10 @@
-import django.http.request as dr
-import django.contrib.auth.models as dam
+import django.http.request as dhreq
+import django.contrib.auth.models as dcam
 import django.db.transaction as ddt
 
 from .. import forms as f
 
-import insta_milligram.constants as c
+import insta_milligram.constants as ic
 import insta_milligram.responses.decorators as ird
 import insta_milligram.forms as if_
 import users.models.users_profiles as umup
@@ -13,14 +13,14 @@ import users.models.users_profiles as umup
 
 
 @ird.check_form(f.UserForm)
-def post(request: dr.HttpRequest):
+def post(request: dhreq.HttpRequest):
     form_data = if_.get_data(f.UserForm(request.POST))
-    user1 = dam.User.objects.filter(email=form_data["email"])
-    user2 = dam.User.objects.filter(username=form_data["username"])
+    user1 = dcam.User.objects.filter(email=form_data["email"])
+    user2 = dcam.User.objects.filter(username=form_data["username"])
     if len(user1) + len(user2):
-        return c.responses.USER_ALREADY_EXISTS
+        return ic.responses.USER_ALREADY_EXISTS
     else:
         with ddt.atomic():
-            user = dam.User.objects.create_user(**form_data)
+            user = dcam.User.objects.create_user(**form_data)
             umup.UserProfile.objects.create(user=user)
-    return c.responses.SUCCESS
+    return ic.responses.SUCCESS
