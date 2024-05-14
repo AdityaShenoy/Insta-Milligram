c12f1f5a386bb91d66f36764da8f10a3d70a2c0b
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 18:01:57 +0530

    test: use list of request inputs

--- insta_milligram/users/tests/test_post.py
+++ insta_milligram/users/tests/test_post.py
@@ -6,6 +6,9 @@ import insta_milligram.responses as ir
 
 
 class TestView(dt.TestCase):
+    def setUp(self):
+        self.signup_request = ic.inputs.SIGNUP_REQUESTS[0]
+
     def test_invalid(self):
         response = self.client.post(ic.urls.USERS)
         ir.assert_equal_responses(response, ic.responses.INVALID_DATA)
@@ -14,30 +17,29 @@ class TestView(dt.TestCase):
         )
 
     def test_valid(self):
-        response = self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
+        response = self.client.post(ic.urls.USERS, self.signup_request)
         ir.assert_equal_responses(response, ic.responses.SUCCESS)
-        user = dcam.User.objects.get(
-            username=ic.inputs.SIGNUP_REQUEST["username"],
-        )
-        assert user.username == ic.inputs.SIGNUP_REQUEST["username"]
-        assert user.check_password(ic.inputs.SIGNUP_REQUEST["password"])
-        assert user.first_name == ic.inputs.SIGNUP_REQUEST["first_name"]
-        assert user.last_name == ic.inputs.SIGNUP_REQUEST["last_name"]
-        assert user.email == ic.inputs.SIGNUP_REQUEST["email"]
+        user = dcam.User.objects.get(username=self.signup_request["username"])
+        for field in self.signup_request:
+            if field == "password":
+                continue
+            assert user.__getattribute__(field) == self.signup_request[field]
+        assert user.check_password(self.signup_request["password"])
         assert user.profile.followers_count == 0  # type: ignore
         assert user.profile.followings_count == 0  # type: ignore
 
     def test_twice_username(self):
         self.client.post(
-            ic.urls.USERS, {**ic.inputs.SIGNUP_REQUEST, "email": "test1@test.com"}
+            ic.urls.USERS,
+            {**self.signup_request, "email": "dummy@dummy.com"},
         )
-        response = self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
+        response = self.client.post(ic.urls.USERS, self.signup_request)
         ir.assert_equal_responses(response, ic.responses.USER_ALREADY_EXISTS)
 
     def test_twice_email(self):
         self.client.post(
             ic.urls.USERS,
-            {**ic.inputs.SIGNUP_REQUEST, "username": "test1"},
+            {**self.signup_request, "username": "dummy"},
         )
-        response = self.client.post(ic.urls.USERS, ic.inputs.SIGNUP_REQUEST)
+        response = self.client.post(ic.urls.USERS, self.signup_request)
         ir.assert_equal_responses(response, ic.responses.USER_ALREADY_EXISTS)
