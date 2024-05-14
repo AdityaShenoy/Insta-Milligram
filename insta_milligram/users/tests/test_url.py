fff6a6ae6b078812885c354cfcebdfb5f9c57449
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-14 22:40:09 +0530

    style: standardize import abbreviations and ordering

--- insta_milligram/users/tests/test_url.py
+++ insta_milligram/users/tests/test_url.py
@@ -1,10 +1,10 @@
 import django.test as dt
 
 from .. import views as v
-import insta_milligram.tests as t
+import insta_milligram.tests as it
 
 
 class TestUrl(dt.SimpleTestCase):
     def test_url_resolution(self):
-        t.test_url_resolution("users", v.UserView)  # type: ignore
-        t.test_url_resolution("users_id", v.UserView, [1])  # type: ignore
+        it.test_url_resolution("users", v.UserView)  # type: ignore
+        it.test_url_resolution("users_id", v.UserView, [1])  # type: ignore
