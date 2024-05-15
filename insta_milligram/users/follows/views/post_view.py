fb91f42226a181de977cb7597c91dae814854958
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-15 20:19:26 +0530

    docs: rename the user variables for self documentation

--- insta_milligram/users/follows/views/post_view.py
+++ insta_milligram/users/follows/views/post_view.py
@@ -15,28 +15,30 @@ import users.models.users_follows as umuf
 @ird.check_authorized()
 @ird.check_form(uff.UserFollowForm)
 def post(request: dhreq.HttpRequest, id: int):
-    user = ag.get_auth_user(request)
+    follower = ag.get_auth_user(request)
     form_data = if_.get_data(uff.UserFollowForm(request.POST))
-    followed_users = dcam.User.objects.filter(pk=form_data["user"])
-    if not followed_users:
+
+    followings = dcam.User.objects.filter(pk=form_data["user"])
+    if not followings:
         return ic.responses.USER_NOT_FOUND
-    followed_user = followed_users[0]
-    if followed_user == user:
+
+    following = followings[0]
+    if following == follower:
         return ic.responses.OPERATION_NOT_ALLOWED
 
     is_already_following = umuf.UserFollow.objects.filter(
-        follower=user, following=followed_user
+        follower=follower, following=following
     ).exists()
     if is_already_following:
         return ic.responses.OPERATION_NOT_ALLOWED
 
     with ddt.atomic():
         umuf.UserFollow.objects.create(
-            follower=user,
-            following=followed_user,
+            follower=follower,
+            following=following,
         )
-        followed_user.profile.followers_count += 1  # type: ignore
-        user.profile.followings_count += 1  # type: ignore
-        followed_user.profile.save()  # type: ignore
-        user.profile.save()  # type: ignore
+        following.profile.followers_count += 1  # type: ignore
+        follower.profile.followings_count += 1  # type: ignore
+        following.profile.save()  # type: ignore
+        follower.profile.save()  # type: ignore
     return ic.responses.SUCCESS
