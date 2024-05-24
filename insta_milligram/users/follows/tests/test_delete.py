f3a38f2cec838c83ff01474ede483221f87e4b35
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-24 11:41:10 +0530

    test: remove redundant test

--- insta_milligram/users/follows/tests/test_delete.py
+++ insta_milligram/users/follows/tests/test_delete.py
@@ -67,25 +67,3 @@ class TestView(dt.TestCase):
         assert not user1.followings.filter(  # type: ignore
             following=user2,
         ).exists()
-
-    def test_valid_2(self):
-        response = self.client.delete(
-            icu.user_id_followings_id(1, 2),
-            headers=self.header1,  # type: ignore
-        )
-        it.assert_equal_responses(response, icr.SUCCESS)
-        user1 = dcam.User.objects.get(pk=1)
-        user2 = dcam.User.objects.get(pk=2)
-        assert user1.profile.followings_count == 0  # type: ignore
-        assert user2.profile.followers_count == 0  # type: ignore
-        follows = umuf.Follow.objects.filter(
-            follower=user1,
-            following=user2,
-        )
-        assert not follows.exists()
-        assert not user2.followers.filter(  # type: ignore
-            follower=user1,
-        ).exists()
-        assert not user1.followings.filter(  # type: ignore
-            following=user2,
-        ).exists()
