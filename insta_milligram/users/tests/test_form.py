e36b054c22c5075e58615c5f416e23aa6838cbd0
Aditya Shenoy <adityashenoy49@gmail.com>
2024-05-24 11:19:21 +0530

    style: upper case constant variable name

--- insta_milligram/users/tests/test_form.py
+++ insta_milligram/users/tests/test_form.py
@@ -6,25 +6,25 @@ import users.forms as uf
 
 class TestForm(dt.SimpleTestCase):
     def setUp(self):
-        self.signup_request = ici.signup_request(1)
-        self.EMPTY_REQUEST = {k: "" for k in self.signup_request}
-        self.BIG_REQUEST = {k: "a" * 51 for k in self.signup_request}
-        self.SMALL_PWD_REQUEST = {**self.signup_request, **ici.SMALL_PASSWORD}
+        self.SIGNUP_REQUEST = ici.signup_request(1)
+        self.EMPTY_REQUEST = {k: "" for k in self.SIGNUP_REQUEST}
+        self.BIG_REQUEST = {k: "a" * 51 for k in self.SIGNUP_REQUEST}
+        self.SMALL_PWD_REQUEST = {**self.SIGNUP_REQUEST, **ici.SMALL_PASSWORD}
 
     def test_missing_field(self):
         form = uf.UserForm({})
         assert not form.is_valid()
-        assert set(form.errors.keys()) == set(self.signup_request.keys())
+        assert set(form.errors.keys()) == set(self.SIGNUP_REQUEST.keys())
 
     def test_empty(self):
         form = uf.UserForm(self.EMPTY_REQUEST)
         assert not form.is_valid()
-        assert set(form.errors.keys()) == set(self.signup_request.keys())
+        assert set(form.errors.keys()) == set(self.SIGNUP_REQUEST.keys())
 
     def test_big(self):
         form = uf.UserForm(self.BIG_REQUEST)
         assert not form.is_valid()
-        assert set(form.errors.keys()) == set(self.signup_request.keys())
+        assert set(form.errors.keys()) == set(self.SIGNUP_REQUEST.keys())
 
     def test_small_password(self):
         form = uf.UserForm(self.SMALL_PWD_REQUEST)
@@ -32,11 +32,11 @@ class TestForm(dt.SimpleTestCase):
         assert "password" in form.errors.keys()
 
     def test_username_with_special_chars(self):
-        form = uf.UserForm({**self.signup_request, **ici.SPECIAL_USERNAME})
+        form = uf.UserForm({**self.SIGNUP_REQUEST, **ici.SPECIAL_USERNAME})
         assert not form.is_valid()
         assert "username" in form.errors.keys()
 
     def test_valid(self):
-        form = uf.UserForm(self.signup_request)
+        form = uf.UserForm(self.SIGNUP_REQUEST)
         assert form.is_valid()
         assert len(form.errors) == 0
