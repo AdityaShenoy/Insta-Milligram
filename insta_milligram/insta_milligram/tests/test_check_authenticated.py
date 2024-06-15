20e5606bea3093e39885a780c6f219d4c882b0be
Aditya Shenoy <adityashenoy49@gmail.com>
2024-06-15 20:38:14 +0530

    test: test check authenticated response decorator

--- insta_milligram/insta_milligram/tests/test_check_authenticated.py
+++ insta_milligram/insta_milligram/tests/test_check_authenticated.py
@@ -11,10 +11,6 @@ class TestView(dt.TestCase):
     def setUp(self):
         self.header = it.signup_and_login(1)
 
-    def test_without_id(self):
-        response = self.client.delete(icu.USERS, headers=self.header)
-        it.assert_equal_responses(response, icr.USER_ID_MISSING)
-
     def test_without_token(self):
         response = self.client.delete(icu.user_id(1))
         it.assert_equal_responses(response, icr.TOKEN_MISSING)
@@ -37,8 +33,3 @@ class TestView(dt.TestCase):
         )
         it.assert_equal_responses(response, icr.INVALID_TOKEN)
         assert len(dcam.User.objects.all()) == 1
-
-    def test_delete_twice(self):
-        self.client.delete(icu.user_id(1), headers=self.header)
-        response = self.client.delete(icu.user_id(1), headers=self.header)
-        it.assert_equal_responses(response, icr.USER_NOT_FOUND)
