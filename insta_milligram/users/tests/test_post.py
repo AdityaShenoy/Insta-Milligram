ad420d622bcad1da48d794b961d2a131385535e2
Aditya Shenoy <adityashenoy49@gmail.com>
2024-06-15 19:27:10 +0530

    style: order test cases as per code

--- insta_milligram/users/tests/test_post.py
+++ insta_milligram/users/tests/test_post.py
@@ -15,18 +15,6 @@ class TestView(dt.TestCase):
             ici.signup_request(1).keys()
         )
 
-    def test_valid(self):
-        response = self.client.post(icu.USERS, ici.signup_request(1))
-        it.assert_equal_responses(response, icr.SUCCESS)
-        user = dcam.User.objects.get(pk=1)
-        for field in ici.signup_request(1):
-            if field == "password":
-                continue
-            assert user.__getattribute__(field) == ici.signup_request(1)[field]
-        assert user.check_password(ici.signup_request(1)["password"])
-        assert user.profile.followers_count == 0  # type: ignore
-        assert user.profile.followings_count == 0  # type: ignore
-
     def test_twice_username(self):
         self.client.post(
             icu.USERS,
@@ -42,3 +30,15 @@ class TestView(dt.TestCase):
         )
         response = self.client.post(icu.USERS, ici.signup_request(1))
         it.assert_equal_responses(response, icr.USER_ALREADY_EXISTS)
+
+    def test_valid(self):
+        response = self.client.post(icu.USERS, ici.signup_request(1))
+        it.assert_equal_responses(response, icr.SUCCESS)
+        user = dcam.User.objects.get(pk=1)
+        for field in ici.signup_request(1):
+            if field == "password":
+                continue
+            assert user.__getattribute__(field) == ici.signup_request(1)[field]
+        assert user.check_password(ici.signup_request(1)["password"])
+        assert user.profile.followers_count == 0  # type: ignore
+        assert user.profile.followings_count == 0  # type: ignore
