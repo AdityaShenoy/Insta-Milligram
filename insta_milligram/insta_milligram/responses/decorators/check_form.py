5402969552634d9919bc8d2b9491927f034097b1
Aditya Shenoy <adityashenoy49@gmail.com>
2024-06-15 19:39:58 +0530

    style: wrap line with more than 80 chars

--- insta_milligram/insta_milligram/responses/decorators/check_form.py
+++ insta_milligram/insta_milligram/responses/decorators/check_form.py
@@ -16,7 +16,10 @@ def check_form(form_class: t.Callable[..., df.Form]):
             request = args[0]
             form = form_class(request.data)
             if not form.is_valid():
-                return ir.create_response(icr.INVALID_DATA, {"errors": form.errors})
+                return ir.create_response(
+                    icr.INVALID_DATA,
+                    {"errors": form.errors},
+                )
 
             return func(*args, **kwargs)
 
