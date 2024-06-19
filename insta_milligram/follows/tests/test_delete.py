2c6b790ea6f639f3ce339885120edaec27e64f6f
Aditya Shenoy <adityashenoy49@gmail.com>
2024-06-19 11:42:41 +0530

    style: wrap lines if number of chars is greater than 80

--- insta_milligram/follows/tests/test_delete.py
+++ insta_milligram/follows/tests/test_delete.py
@@ -14,7 +14,9 @@ class TestView(dt.TestCase):
         self.header1 = it.signup_and_login(2)
         it.signup_and_login(3)
         self.client.post(
-            icu.user_id_followings(1), ici.follow_request(2), headers=self.header
+            icu.user_id_followings(1),
+            ici.follow_request(2),
+            headers=self.header,
         )
 
     def test_without_login(self):
@@ -46,6 +48,28 @@ class TestView(dt.TestCase):
     def test_valid(self):
         response = self.client.delete(
             icu.user_id_followings_id(1, 2), headers=self.header
+        )
+        it.assert_equal_responses(response, icr.SUCCESS)
+        user1 = dcam.User.objects.get(pk=1)
+        user2 = dcam.User.objects.get(pk=2)
+        assert user1.profile.followings_count == 0  # type: ignore
+        assert user2.profile.followers_count == 0  # type: ignore
+        follows = fm.Follow.objects.filter(follower=user1, following=user2)
+        assert not follows.exists()
+        assert not user2.followers.filter(  # type: ignore
+            follower=user1,
+        ).exists()
+        assert not user1.followings.filter(  # type: ignore
+            following=user2,
+        ).exists()
+
+        self.client.post(
+            icu.user_id_followings(1),
+            ici.follow_request(2),
+            headers=self.header,
+        )
+        response = self.client.delete(
+            icu.user_id_followings_id(1, 2), headers=self.header1
         )
         it.assert_equal_responses(response, icr.SUCCESS)
         user1 = dcam.User.objects.get(pk=1)
